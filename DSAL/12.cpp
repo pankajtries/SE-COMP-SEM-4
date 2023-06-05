1d21139caacfee5f44a935a8fc88cd69f8156341
Pankaj Agarwal <98219223+PankajAgarwalS@users.noreply.github.com>
2023-06-05 11:54:06 +0530

    Update 12.cpp

--- DSAL/12.cpp
+++ DSAL/12.cpp
@@ -1,101 +1,96 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
 using namespace std;
 
-class Employee {
-private:
+struct Employee {
     int empId;
     char name[50];
     char designation[50];
     double salary;
-
-public:
-    void addEmployee() {
-        ofstream file("employees.dat", ios::binary | ios::app);
-        if (!file) {
-            cout << "Error opening file!" << endl;
-            return;
-        }
-        cout << "Enter Employee ID: ";
-        cin >> empId;
-        cout << "Enter Employee Name: ";
-        cin.ignore();
-        cin.getline(name, 50);
-        cout << "Enter Employee Designation: ";
-        cin.getline(designation, 50);
-        cout << "Enter Employee Salary: ";
-        cin >> salary;
-
-        file.write(reinterpret_cast<char*>(this), sizeof(Employee));
-
-        file.close();
-    }
-
-    void deleteEmployee(int empId) {
-        fstream file("employees.dat", ios::binary | ios::in | ios::out);
-        if (!file) {
-            cout << "Error opening file!" << endl;
-            return;
-        }
-
-        Employee emp;
-        bool found = false;
-        while (file.read(reinterpret_cast<char*>(&emp), sizeof(Employee))) {
-            if (emp.empId == empId) {
-                found = true;
-                break;
-            }
-        }
-
-        if (found) {
-            long pos = file.tellg();
-            file.seekp(pos - sizeof(Employee));  // Move the write position
-            Employee blankEmp;
-            file.write(reinterpret_cast<char*>(&blankEmp), sizeof(Employee));  // Overwrite with blank record
-            cout << "Employee with ID " << empId << " deleted successfully." << endl;
-        } else {
-            cout << "Employee with ID " << empId << " not found." << endl;
-        }
-
-        file.close();
-    }
-
-
-    void displayEmployee(int empId) {
-        ifstream file("employees.dat", ios::binary);
-        if (!file) {
-            cout << "Error opening file!" << endl;
-            return;
-        }
-
-        bool found = false;
-        while (!file.eof()) {
-            file.read(reinterpret_cast<char*>(this), sizeof(Employee));
-            if (this->empId == empId) {
-                found = true;
-                break;
-            }
-        }
-
-        if (found) {
-            cout << "Employee ID: " << empId << endl;
-            cout << "Name: " << name << endl;
-            cout << "Designation: " << designation << endl;
-            cout << "Salary: " << salary << endl;
-        } else {
-            cout << "Employee with ID " << empId << " not found." << endl;
-        }
-
-        file.close();
-    }
 };
+
+void addEmployee() {
+    ofstream file("employees.dat", ios::binary | ios::app);
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return;
+    }
+
+    Employee emp;
+    cout << "Enter Employee ID: ";
+    cin >> emp.empId;
+    cout << "Enter Employee Name: ";
+    cin.ignore();
+    cin.getline(emp.name, 50);
+    cout << "Enter Employee Designation: ";
+    cin.getline(emp.designation, 50);
+    cout << "Enter Employee Salary: ";
+    cin >> emp.salary;
+
+    file.write(reinterpret_cast<char*>(&emp), sizeof(Employee));
+    file.close();
+}
+
+void deleteEmployee(int empId) {
+    fstream file("employees.dat", ios::binary | ios::in | ios::out);
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return;
+    }
+
+    Employee emp;
+    bool found = false;
+    while (file.read(reinterpret_cast<char*>(&emp), sizeof(Employee))) {
+        if (emp.empId == empId) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        emp.empId = -1; // Mark the record as deleted
+        long pos = file.tellg();
+        file.seekp(pos - sizeof(Employee)); // Move the write position
+        file.write(reinterpret_cast<char*>(&emp), sizeof(Employee)); // Overwrite with updated record
+        cout << "Employee with ID " << empId << " deleted successfully." << endl;
+    } else {
+        cout << "Employee with ID " << empId << " not found." << endl;
+    }
+
+    file.close();
+}
+
+void displayEmployee(int empId) {
+    ifstream file("employees.dat", ios::binary);
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return;
+    }
+
+    Employee emp;
+    bool found = false;
+    while (file.read(reinterpret_cast<char*>(&emp), sizeof(Employee))) {
+        if (emp.empId == empId && emp.empId != -1) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        cout << "Employee ID: " << empId << endl;
+        cout << "Name: " << emp.name << endl;
+        cout << "Designation: " << emp.designation << endl;
+        cout << "Salary: " << emp.salary << endl;
+    } else {
+        cout << "Employee with ID " << empId << " not found." << endl;
+    }
+
+    file.close();
+}
 
 int main() {
     int choice;
     int empId;
-
-    Employee emp;
 
     do {
         cout << "1. Add Employee" << endl;
@@ -107,17 +102,17 @@ int main() {
 
         switch (choice) {
             case 1:
-                emp.addEmployee();
+                addEmployee();
                 break;
             case 2:
                 cout << "Enter Employee ID to delete: ";
                 cin >> empId;
-                emp.deleteEmployee(empId);
+                deleteEmployee(empId);
                 break;
             case 3:
                 cout << "Enter Employee ID to display: ";
                 cin >> empId;
-                emp.displayEmployee(empId);
+                displayEmployee(empId);
                 break;
             case 4:
                 cout << "Exiting program." << endl;
@@ -127,5 +122,6 @@ int main() {
                 break;
         }
     } while (choice != 4);
+
     return 0;
 }
