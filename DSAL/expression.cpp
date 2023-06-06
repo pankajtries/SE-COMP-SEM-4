d6615131c8f301d101414e52c45a6825cb109f39
Pankaj Agarwal <98219223+PankajAgarwalS@users.noreply.github.com>
2023-06-06 10:41:02 +0530

    Update expression.cpp

--- DSAL/expression.cpp
+++ DSAL/expression.cpp
@@ -1,79 +1,102 @@
-#include<iostream>
-#include<stack>
-#include<string>
+#include <iostream>
+#include <map>
+#include <stack>
 
 using namespace std;
 
-// Structure to represent a node in the expression tree
-struct TreeNode {
-    char val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(char x) : val(x), left(nullptr), right(nullptr) {}
+class node {
+   public:
+    char data;
+    node *left, *right;
+    node() {
+        left = NULL;
+        right = NULL;
+    }
+
+    node(char ch) {
+        data = ch;
+        left = NULL;
+        right = NULL;
+    }
 };
 
-// Function to construct the expression tree from the given expression
-TreeNode* buildExpressionTree(string expression) {
-    stack<TreeNode*> st;
-    for(int i = 0; i < expression.length(); i++) {
-        if(expression[i] == ' ') {
-            continue;
+class tree {
+   private:
+    node *root;
+    stack<node *> nodestack;
+
+    bool isOperator(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                return 1;
+                break;
+            default:
+                return 0;
+                break;
         }
-        if(expression[i] >= '0' && expression[i] <= '9' || isalpha(expression[i])) {
-            TreeNode* node = new TreeNode(expression[i]);
-            st.push(node);
+    }
+
+    void insert(char ch) {
+        if (isOperator(ch)) {
+            node *newnode = new node(ch);
+            newnode->left = nodestack.top();
+            nodestack.pop();
+            newnode->right = nodestack.top();
+            nodestack.pop();
+            nodestack.push(newnode);
         } else {
-            TreeNode* right = st.top();
-            st.pop();
-            TreeNode* left = st.top();
-            st.pop();
-            TreeNode* node = new TreeNode(expression[i]);
-            node->left = left;
-            node->right = right;
-            st.push(node);
+            node *newnode = new node(ch);
+            nodestack.push(newnode);
         }
     }
-    return st.top();
-}
 
-// Function to traverse the expression tree using postorder traversal (non-recursive)
-void postorderTraversal(TreeNode* root) {
-    stack<TreeNode*> st1, st2;
-    st1.push(root);
-    while(!st1.empty()) {
-        TreeNode* node = st1.top();
-        st1.pop();
-        st2.push(node);
-        if(node->left != nullptr) {
-            st1.push(node->left);
+   public:
+    void input_prefix(string exp) {
+        for (int i = exp.length() - 1; i > -1; i--) {
+            insert(exp[i]);
         }
-        if(node->right != nullptr) {
-            st1.push(node->right);
+        root = nodestack.top();
+        nodestack.pop();
+    }
+
+    void display_postfix() {
+        node *temp = root;
+
+        stack<node *> stack1, stack2;
+
+        stack1.push(root);
+
+        while (!stack1.empty()) {
+            temp = stack1.top();
+            stack1.pop();
+            stack2.push(temp);
+            if (temp->left != NULL) {
+                stack1.push(temp->left);
+            }
+            if (temp->right != NULL) {
+                stack1.push(temp->right);
+            }
         }
-    }
-    while(!st2.empty()) {
-        cout << st2.top()->val << " ";
-        st2.pop();
-    }
-}
+        while (!stack2.empty()) {
+            temp = stack2.top();
+            stack2.pop();
+            cout << temp->data;
+        }
 
-// Function to delete the entire expression tree
-void deleteTree(TreeNode* root) {
-    if(root == nullptr) {
-        return;
+        cout << endl;
     }
-    deleteTree(root->left);
-    deleteTree(root->right);
-    delete root;
-}
+};
 
-// Main function to test the code
 int main() {
-    string expression = "a - b * c - d / e + f";
-    TreeNode* root = buildExpressionTree(expression);
-    cout << "Postorder traversal: ";
-    postorderTraversal(root);
-    cout << endl;
-    deleteTree(root);
+    tree exp_tree;
+    string e;
+    cout << "Enter Prefix Expression:";
+    cin >> e;
+    exp_tree.input_prefix(e);
+    exp_tree.display_postfix();
     return 0;
 }
+// +--a*bc/def
